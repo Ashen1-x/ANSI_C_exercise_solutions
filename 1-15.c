bb9e50134af4b7ceb0addc41d7f930a39c5d8d2e
Ashen1 <beelzabul@proton.me>
2025-03-20 13:54:19 -0700

    my temp converter attempt

--- 1-15.c
+++ 1-15.c
@@ -1,22 +1,69 @@
 #include<stdio.h>
 
-double fahrenheit_to_celsius(double fahrenheit) {
+int fahr_to_cels(int fahr, double cels){
+    int f;
+    double c;
 
-    return(fahrenheit -32) * 5.0/9.0;
+    printf("Enter the temp in F*: ", f);
+    scanf("%d", &f);
+    printf("Fahrenheit\tCelsius\n%3d\t\t%6.1lf\n",f, c = ((5.0/9.0) * (f - 32.0)));
+
+    
 
 
 }
 
+
+int cels_to_fahr(double cels, int fahr){
+    int f;
+    double c;
+
+    printf("Enter the temp in C*: ", c);
+    scanf("%lf", &c);
+
+    printf("Celsius\tFahrenheit\n%6.1lf\t\t%3d\n", c, f = ((c * 9.0/5.0) + 32.0));
+
+
+
+}
+
+
 int main(){
 
+    int fahr;
+    double cels;
+    int ans;
 
-    double fahrenheit;
+    while(1){
 
-    printf("Enter temperature in fahrenheit, please: ");
-    scanf("%lf", &fahrenheit);
+        printf("press 1 for f2c |or|0 for c2f |or| cntrl + c to exit: ", ans);
+        scanf("%d", &ans);
 
-    printf("Temp in Celsius: %.2f\n", fahrenheit_to_celsius(fahrenheit));
+        if(ans == 1){
+
+
+            fahr_to_cels(fahr, cels);
+        }
+
+
+        else{
+            cels_to_fahr(cels, fahr);
+        }
+
+
+
+
+
+
+
+    }
+
+
+
     return 0;
+
+
+
 
 
 }
--- 1-7.c
+++ 1-7.c
@@ -3,13 +3,21 @@
 int main(){
 
 
-    int c;
+    int c = 10;
+    int *ptr;
+    ptr = &c;
 
-    while((c = getchar()) != EOF)
-        putchar(c);
-
-    printf("EOF is here. The value of EOOF is: %d\n", EOF);
+    printf("Address of c: %p\n", &c);
+    printf("Value of ptr: %p\n", ptr);
+    printf("Value of c: %d\n", c);
+    printf("Value pointed to by ptr: %d\n", *ptr);
     return 0;
+
+    // while((c = getchar()) != EOF)
+    //     putchar(c);
+
+    // printf("EOF is here. The value of EOOF is: %d\n", EOF);
+    // return 0;
 
 
 }
